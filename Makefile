229dd9f0307f615a848c8aebac9648613dd490a0
SaaFomenko <s.fomenko@list.ru>
2022-11-09 19:47:14 +0300

    Fix homework, add shifts and Makefile.

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	gcc  main.cpp filein.cpp fileout.cpp arrinit.cpp handlerarr.cpp -lstdc++ -o main.out
+
--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main() {
 	const char* out_file = { "./out.txt" };
   const std::string msg = "Данные были преобразованы и записаны в файл: ";
 
-	int size = 0;
+	int size1 = 0;
+	int size2 = 0;
 
 	std::ifstream fin = fileIn(in_file);
 	bool check = fin.is_open();
@@ -17,17 +18,24 @@ int main() {
 	check = fout.is_open();
 	if (!check) return 1;
 
-	while (fin >> size)
-	{
-		int* arr = arrInit(&fin, size);
-		handlerArr(arr, size, &fout);
- 
-		delete[] arr;
-		arr = nullptr;
-	}
+	fin >> size1;
+	int* arr1 = arrInit(&fin, size1);
+
+	fin >> size2;
+	int* arr2 = arrInit(&fin, size1);
 
 	fin.close();
+
+	handlerArr(arr2, size2, &fout);
+	handlerArr(arr1, size1, &fout);
+
 	fout.close();
+
+	delete[] arr1;
+	arr1 = nullptr;
+	delete[] arr2;
+	arr2 = nullptr;
+
 
 	std::cout << msg << out_file << std::endl;
 
